30af973f37899811dc56d7063445fb064ba12437
Thiago Ferrer <thi-ferrer@live.com>
2025-08-21 15:40:19 -0300

    update

--- src/main/java/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/main/java/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -1,4 +1,51 @@
 package main.java.set.OperacoesBasicas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConjuntoConvidados {
+    //atributo:
+    private final Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidados 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidados 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidados 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidados 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+    }
 }
