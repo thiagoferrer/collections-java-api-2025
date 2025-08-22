77c21597def211ae3818064178373932f1b152ea
Thiago Ferrer <thi-ferrer@live.com>
2025-08-21 22:40:16 -0300

    CadastroProdutos

---
+++ src/main/java/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,42 @@
+package main.java.set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutoPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(1l, "Produto 05", 15.00, 5);
+        cadastroProdutos.adicionarProduto(2l, "Produto 00", 20.00, 10);
+        cadastroProdutos.adicionarProduto(1l, "Produto 03", 10.00, 2);
+        cadastroProdutos.adicionarProduto(9l, "Produto 09", 2.00, 2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutoPorPreco());
+    }
+}
